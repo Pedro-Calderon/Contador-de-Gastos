34005622e1c9fb7413e7e887d0a0fe8ef52330ba
Pedro-Calderon <s18120164@alumnos.itsur.edu.mx>
2025-03-10 23:49:28 -0600

    Create UI Gastos.jsx

--- front-end/src/Views/Gastos.jsx
+++ front-end/src/Views/Gastos.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Plus } from 'lucide-react'; 
 
 function Gastos() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const gastado = 0.00.toFixed(2);
+    return (
+        <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 relative'>
+            <div className='flex flex-col items-center justify-center border border-gray-400 border-opacity-50 p-9 rounded-md w-full max-w-md'>
+                <h1 className='text-4xl font-bold mb-4'>Contador de Gastos</h1>
+                <div className='flex items-center'>
+                    <div className='bg-red-500 p-4 mr-4 text-white'>Aqui va la grafica</div>
+                    <div className='flex flex-col items-center'>
+                        <Button variant="outline" className="w-full">Reset APP</Button>
+                        <h1 className='text-blue-500 font-bold mt-4'>Presupuesto: <label className='font-bold text-black'>${gastado}</label></h1>
+                        <h1 className='text-blue-500 font-bold mt-4'>Disponible: <label className='font-bold text-black'>${gastado}</label></h1>
+                        <h1 className='text-blue-500 font-bold mt-4'>Gastado: <label className='font-bold text-black'>${gastado}</label></h1>
+                    </div>
+                </div>
+            </div>
+            
+            <div className='flex flex-col items-center justify-center border border-gray-400 border-opacity-50 p-9 rounded-md mt-8 w-full max-w-md'>
+                <div className='flex items-center w-full'>
+                    <div className='mr-4 w-full'>Filtrar gastos</div>
+                    <select className='border border-gray-300 rounded-md p-2 mb-4 w-full'>
+                        <option value='0' selected>--Seleccione--</option>
+                        <option value='1'>Ahorro</option>
+                        <option value='2'>Comida</option>
+                        <option value='3'>Casa</option>
+                        <option value='4'>Gastos Varios</option>
+                        <option value='5'>Ocio</option>
+                        <option value='6'>Salud</option>
+                        <option value='7'>Suscripciones</option>
+                    </select>
+                </div>
+            </div>
+
+            <div className='flex flex-col items-center justify-center border border-gray-400 border-opacity-50 p-9 rounded-md mt-8 w-full max-w-md'>
+                <div className='flex items-center justify-center w-full'>
+                    <h1 className='font-bold mb-4'>Listado de Gastos</h1>
+                </div>
+            </div>
+
+            <Button className="rounded-full w-12 h-12 p-0 flex items-center justify-center bg-black text-white fixed bottom-8 right-8">
+                <Plus className="h-6 w-6" /> 
+            </Button>
+        </div>
+    )
 }
 
 export default Gastos
